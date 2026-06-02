0c8bb3f9d6f43b467b1026ef96cf87c4b8802a24
nwntaspap <nwntaspapmichael@gmail.com>
2026-06-02 18:13:33 +0300

    setup Vite, ESLint, and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  prettierConfig,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      prettier,
+    },
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]);
